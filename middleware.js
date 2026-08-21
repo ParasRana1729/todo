66743fa413476554f29981684806aec55534d4dd
ParasRana1729 <parasrana1728@gmail.com>
2026-08-22 01:51:59 +0530

    added authMiddleware

---
+++ middleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+function authMiddleware(req, res, next) {
+  const token = req.headers.token
+  const decoded = jwt.verify(token, 'key')
+
+  if (decoded.userId) {
+    req.userId = decoded.userId
+    next()
+  } else {
+    res.status('403').json({
+      message: "Token is invalid or not found"
+    })
+  }
+}
+
+module.exports = {
+  authMiddleware
+}
